fd7c1d652bdf3ba90233a51831f87c3812533b81
Maya <mjoudat@yandex.ru>
2024-08-29 17:57:43 +0300

    add get_ping_result_by_url function & refactor get_ping_results

--- crud.py
+++ crud.py
@@ -2,11 +2,14 @@ from sqlalchemy.orm import Session
 import models, schemas
 
 def get_ping_results(db: Session):
-    return db.query(models.PingResult)
+    return db.query(models.PingResult).all()
     # TODO refactor to return paginated data
 
-def get_ping_result_by_id(db: Session, record_id:int):
+def get_ping_result_by_id(db: Session, record_id: int):
     return db.get(models.PingResult, record_id)
+
+def get_ping_results_by_url(db: Session, url: str):
+    return db.query(models.PingResult).where(models.PingResult.url == url).all()
 
 def add_ping_result(db: Session, pr: schemas.PingResultBase):
     record = models.PingResult(url=pr.url, ip=pr.ip, packets_sent=pr.packets_sent, packets_recieved=pr.packets_recieved, avg_ping_time=pr.avg_ping_time, time_sent=pr.time_sent)
