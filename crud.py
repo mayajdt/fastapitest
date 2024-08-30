8f617d186ebe488cac4a8eeabcc6b2cb7100c608
Maya <mjoudat@yandex.ru>
2024-08-30 17:47:21 +0300

    refactor code NOT DONE

--- crud.py
+++ crud.py
@@ -1,12 +1,37 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+import sqlalchemy.exc as exc
 import models, schemas
+from sqlalchemy import text
 
-def get_ping_results(db: Session):
-    return db.query(models.PingResult).all()
+def get_ping_results(db: Session) -> list[models.PingResult]: 
+    stmt = text("SELECT * FROM pingResult")
+    result = db.execute(stmt)
+    for row in result:
+        pass
+
+    return
+    # statement = select(models.PingResult)
+    # try:
+    #     result = db.execute(statement).all()
+    #     for row in result:
+    #         print(row)
+    # except exc.StatementError:
+    #     return exc.StatementError
+    # except exc.NoSuchTableError:
+    #     return exc.NoSuchTableError
+    
+    # return result
+    
     # TODO refactor to return paginated data
 
 def get_ping_result_by_id(db: Session, record_id: int):
-    return db.get(models.PingResult, record_id)
+    statement = select(models.PingResult).where(id == record_id)
+    # try:
+    #     result = db.execute(statement)
+    # # except exc.StatementError:
+    return
+
 
 def get_ping_results_by_url(db: Session, url: str):
     return db.query(models.PingResult).where(models.PingResult.url == url).all()
