4abddab18336ce37403c08ea7172b975e1c51ab6
Maya <mjoudat@yandex.ru>
2024-08-30 17:48:58 +0300

    add get endpoints

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-import models
-from database import SessionLocal, engine
-import test
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+import models, handlers.testhandlers as test, handlers.crudhandlers as crud
+from database import engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.post("/test")
-def s_test(db: Session = Depends(get_db)):
-    return test.test_impl(db)
+def s_test():
+    return test.test_impl()
 
 @app.post("/a-test")
-async def a_test(db: Session = Depends(get_db)):
-    return test.test_impl(db)
+async def a_test():
+    return test.test_impl()
+
+@app.get("/result")
+def result():
+    return crud.get_results()
+
+@app.get("/result/{record_id}")
+def result_by_id(record_id: str):
+    record_id_int = int(record_id)
+    return crud.get_result_by_id(record_id=record_id_int)
+
+@app.get("/result/")
+def result_by_url(url: str):
+    return crud.get_results_by_url(url=url)
