c410f1d24b9fa036469cbe30673be32338ad57d9
oogabooga27 <mayajoudat@outlook.com>
2024-09-02 02:02:24 +0300

    refactor DAO get implementations

--- crud.py
+++ crud.py
@@ -1,47 +1,147 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 import sqlalchemy.exc as exc
-import models, schemas
-from sqlalchemy import text
+from sqlalchemy import text, insert
+from models import PingResult
+from schemas import PingResultBase
 
-def get_ping_results(db: Session) -> list[models.PingResult]: 
-    stmt = text("SELECT * FROM pingResult")
-    result = db.execute(stmt)
-    for row in result:
-        pass
+def get_ping_results(db: Session) -> list[dict] | dict:
+    '''
+    Will attempt to fetch all records from PingResult.
 
-    return
-    # statement = select(models.PingResult)
-    # try:
-    #     result = db.execute(statement).all()
-    #     for row in result:
-    #         print(row)
-    # except exc.StatementError:
-    #     return exc.StatementError
-    # except exc.NoSuchTableError:
-    #     return exc.NoSuchTableError
+    If successful, function will return list[dict]
+
+    If not, function will return following dict object:
+
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    try:
+        res = db.execute(select(PingResult))
+    except Exception as err:
+        return dict(error_name= err.__class__.__name__, error_desc="Couldn't fetch ping results, check logs")
+    else:
+        ret = []
+        for r in res:
+            ret.append(r._asdict())
+        return ret
+
+def get_ping_result_by_id(db: Session, record_id: int) -> dict:
+    '''
+    Will attempt to fetch a single record by its id from database.
+
+    If successful, function will return a dict with the record data
+
+    If not, function will return following dict object:
+
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    record = db.get(PingResult, record_id)
+    if not record:
+        return dict(
+            error_name="NoRecordFound",
+            error_desc="No record found in database"
+        )
+    return record.__dict__
+
+
+def get_ping_results_by_url(db: Session, url: str) -> list[dict] | dict:
+    '''
+    Will attempt to fetch a list of records filtered by url.
+
+    If successful, function will return a list of dicts with the record data
+
+    If not, function will return following dict object:
+
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    try:
+         res = db.execute(
+            select(PingResult).
+            where(PingResult.url == url)
+        )
+    except Exception as err:
+        return dict(error_name= err.__class__.__name__, error_desc="Couldn't fetch ping results, check logs")
+    else:
+        ret = []
+        for r in res:
+            ret.append(r._asdict())
+        return ret
+
+def add_ping_result(db: Session, data: PingResult) -> None | dict:
+    '''
+    Will attempt to persist a single record to database.
+
+    If successful, function will return None
+
+    If not, function will return following dict object:
+
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    try:
+        db.add(data)
+        db.commit()
+    except Exception as err:
+        return dict(error_name= err.__class__.__name__, error_desc="Couldn't bulk insert ping results, check logs")
+    else:
+        return None
+
+def add_bulk_ping_result(db: Session, data: list[PingResultBase]) -> None | dict:
+    '''
+    Will attempt to persist a bulk of records to database.
+
+    If successful, function will return None
+
+    If not, function will return following dict object:
+
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    records = []
+    for d in data:
+        records.append(d.model_dump())
+    try:
+        db.execute(
+            insert(PingResult),
+            records
+        )
+    except Exception as err:
+        return dict(error_name= err.__class__.__name__, error_desc="Couldn't bulk insert ping results, check logs")
+    else:
+        db.commit()
+        return None
     
-    # return result
-    
-    # TODO refactor to return paginated data
+def delete_ping_result_by_id(db: Session, record_id) -> None | dict:
+    '''
+    Will attempt to delete a single record from database by id.
 
-def get_ping_result_by_id(db: Session, record_id: int):
-    statement = select(models.PingResult).where(id == record_id)
-    # try:
-    #     result = db.execute(statement)
-    # # except exc.StatementError:
-    return
+    If successful, function will return None
 
+    If not, function will return following dict object:
 
-def get_ping_results_by_url(db: Session, url: str):
-    return db.query(models.PingResult).where(models.PingResult.url == url).all()
-
-def add_ping_result(db: Session, pr: schemas.PingResultBase):
-    record = models.PingResult(url=pr.url, ip=pr.ip, packets_sent=pr.packets_sent, packets_recieved=pr.packets_recieved, avg_ping_time=pr.avg_ping_time, time_sent=pr.time_sent)
-    db.add(record)
-    db.commit()
-
-def delete_ping_result_by_id(db: Session, record_id):
-    recordToDelete = db.get(models.PingResult, record_id)
-    db.delete(recordToDelete)
-    db.commit()
+        dict(
+            error_name,
+            error_desc
+        )
+    '''
+    try:
+        recordToDelete = db.get(PingResult, record_id)
+        db.delete(recordToDelete)
+        db.commit()
+    except Exception as err:
+        return dict(error_name= err.__class__.__name__, error_desc="Couldn't delete ping results, check logs")
+    else:
+        return None
