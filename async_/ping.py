8a605b1f8d56a0b76c04c7d8ad1ace65c94e5d29
Maya <mjoudat@yandex.ru>
2024-09-05 16:44:07 +0300

    refactor func to run faster + segregate cpu bound funcs from main func

--- async_/ping.py
+++ async_/ping.py
@@ -1,10 +1,38 @@
 from schemas.schemas import PingResultBase, ErrorResultBase
-from datetime import datetime   
-from icmplib import async_ping, async_resolve, Host
-from icmplib.exceptions import NameLookupError, SocketPermissionError, SocketAddressError, ICMPSocketError
+from datetime import datetime
+from icmplib import async_ping, Host
+from icmplib.exceptions import ICMPLibError
 from schemas.schemas import PingResultBase
+import time
+import asyncio
+from itertools import chain
 
-async def async_ping_t(urls: list[str]) -> tuple[list[PingResultBase], list[dict]]:
+class PingResRaw:
+    def __init__(self, url: str, ping_data: Host, time_sent: datetime):
+        self.url= url
+        self.ping_data= ping_data
+        self.time_sent= time_sent
+
+def gen_ping_result_objs(l: chain[PingResRaw]) -> list[PingResultBase]:
+    res = []
+    for el in l:
+        res.append(
+            PingResultBase(
+                url=el.url,
+                ip=el.ping_data.address,
+                avg_ping_time=el.ping_data.avg_rtt,
+                packets_sent=el.ping_data.packets_sent,
+                packets_recieved=el.ping_data.packets_received,
+                time_sent=el.time_sent
+            )
+        )
+    return res
+
+def chunks(l, n):
+    for i in range(0, n):
+        yield l[i::n]
+
+async def async_ping_t(urls: list[str]) -> tuple[chain[PingResRaw], chain[tuple]]:
     '''
     Ping urls in the passed list
 
@@ -20,66 +48,40 @@ async def async_ping_t(urls: list[str]) -> tuple[list[PingResultBase], list[dict
 
     Function will return two lists: one for successful pings and one for unsuccessful
 
-    This function uses async_multiping() and async_resolve, which are non-blocking functions
+    This function uses async_ping() and async_resolve, which are non-blocking functions
     '''
+
+    start = time.perf_counter()
 
     time_sent = datetime.now()
 
-    ok = []
-    resolved_ips = []
-    failed = []
-
-    for url in urls:
-        try:
-            ips = await async_resolve(url)
-        except NameLookupError:
-            failed.append(dict(url= url, error_desc="Could not resolve host"))
-        else:
-            try:
-                res = await async_ping(address=ips[0], count=1, privileged=False)
-            except SocketPermissionError:
-                failed.append(
-                    dict(
-                        url= url, 
-                        error_name=SocketPermissionError.__name__,
-                        error_desc="Socket does not have privilige"
-                        )
-                )
-            except SocketAddressError:
-                failed.append(
-                    dict(
-                        url= url, 
-                        error_name= SocketAddressError.__name__,
-                        error_desc="Address cannot be assigned to socket"
-                        )
-                )
-            except ICMPSocketError:
-                failed.append(
-                    dict(
-                        url= url,
-                        error_name=ICMPSocketError.__name__,
-                        error_desc="An error with the socket occurred"
-                        )
-                )
-            else:
-                if res.packets_received != res.packets_sent:
-                    failed.append(
-                        dict(
-                            url= url,
-                            error_name="NoPacketsRecieved",
-                            error_desc="Didnt recieve any packets back, probably dead"
-                            )
-                    )
+    async def foo(urls: list) -> tuple[list, list]:
+        ok = []
+        failed = []
+        for url in urls:
+                try:
+                    res = await async_ping(address=url, count=1, privileged=False, timeout=1)
+                except ICMPLibError as err:
+                    failed.append(tuple([url, err]))
                 else:
-                    ok.append(
-                        PingResultBase(
-                            url=url,
-                            ip=res.address,
-                            packets_sent=res.packets_sent,
-                            packets_recieved=res.packets_received,
-                            avg_ping_time=res.avg_rtt,
-                            time_sent=time_sent
-                        )
-                    )
+                    ok.append(PingResRaw(url=url, ping_data=res, time_sent=time_sent))
+        return ok, failed
+
+    list1, list2, list3, list4 = chunks(urls, 4)
+
+    res1, res2, res3, res4 = await asyncio.gather(
+        foo(list1),
+        foo(list2),
+        foo(list3),
+        foo(list4),
+    )
+
+    ok = chain(res1[0], res2[0], res3[0], res4[0])
+    failed = chain(res1[1], res2[1], res3[1], res4[1])
+
+    stop = time.perf_counter()
+
+    time_spent = stop - start
+    print(f'pinging all {len(urls)} urls took {time_spent} seconds')
             
     return ok, failed
